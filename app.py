bd124d6c5e3be032abfac5ef04563b2ec2c8e91f
RobertoJustino <robertojustino@live.fr>
2023-01-11 11:28:11 -0500

    notebook_and_files

---
+++ app.py
@@ -0,0 +1,20 @@
+import joblib
+import gradio as gr
+
+# Load the model
+model_bayes = load('filename.joblib')
+
+# Prediction function
+def make_prediction(user_sentence):
+  
+  prediction = model_bayes.predict([user_sentence])
+  dict = {1: 'Negative', 2: 'Neutral', 3: 'Positive'}
+  return dict[prediction[0]]
+
+title = "Sentiment Analysis MyAnimeList Reviews"
+description = "<p style='text-align: center'>Identifier si un commentaire dans MyAnimeList est positif, neutre ou négatif.<br/> Permet de connaître rapidement le sentiment globale que dégage un avis sur le site.</p>"
+examples = ["I liked this show but now I do not love this since the last season. The animation is terrible and the drawings are awful. I don't recommend this show to anyone.", "This is amazing !"]
+
+app = gr.Interface(fn=make_prediction, title=title, description=description, examples=examples, inputs=gr.TextArea(), outputs=gr.Label(num_top_classes=3))
+
+app.launch(share=True)
